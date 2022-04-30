8d92f75e702fcd87a749d864450764dc06da1f75
yczcc <yczcc@hotmail.com>
2022-04-30 23:00:51 +0800

    merge from commit bc2d8d510e804a425f490fe20bfd2e7615591663

--- api_weather.py
+++ api_weather.py
@@ -24,65 +24,67 @@ class Weather:
         """
         try:
             with open(
-                    os.path.join(os.path.dirname(__file__), "city.json"),
-                    "r",
-                    encoding="utf-8",
+                os.path.join(os.path.dirname(__file__), "city.json"),
+                "r",
+                encoding="utf-8",
             ) as city_file:
                 city_map = json.loads(city_file.read())
                 if not city_map:
                     raise FileNotFoundError
         except FileNotFoundError:
-            resp = requests.get("https://fastly.jsdelivr.net/gh/yczcc/checkinpanel@master/city.json")
-            if resp.status_code == 200:
-                city_map = resp.json()
+            r = requests.get(
+                "https://fastly.jsdelivr.net/gh/Oreomeow/checkinpanel@master/city.json"
+            )
+            if r.status_code == 200:
+                city_map = r.json()
                 with open(
-                        os.path.join(os.path.dirname(__file__), "city.json"),
-                        "w",
-                        encoding="utf-8",
+                    os.path.join(os.path.dirname(__file__), "city.json"),
+                    "w",
+                    encoding="utf-8",
                 ) as city_file:
-                    json.dump(city_map, city_file,ensure_ascii=False)
+                    json.dump(city_map, city_file, ensure_ascii=False)
             else:
                 return "下载 city.json 失败！"
         msg_all = ""
         for city_name in self.check_items:
             city_code = city_map.get(city_name, "101020100")
             weather_url = f"http://t.weather.itboy.net/api/weather/city/{city_code}"
-            resp = requests.get(url=weather_url)
-            if resp.status_code == 200 and resp.json().get("status") == 200:
-                d = resp.json()
+            r = requests.get(url=weather_url)
+            if r.status_code == 200 and r.json().get("status") == 200:
+                d = r.json()
                 msg = (
-                        "\n城市："
-                        + d["cityInfo"]["parent"]
-                        + " "
-                        + d["cityInfo"]["city"]
-                        + "\n日期："
-                        + d["data"]["forecast"][0]["ymd"]
-                        + " "
-                        + d["data"]["forecast"][0]["week"]
-                        + "\n天气："
-                        + d["data"]["forecast"][0]["type"]
-                        + "\n温度："
-                        + d["data"]["forecast"][0]["high"]
-                        + " "
-                        + d["data"]["forecast"][0]["low"]
-                        + "\n湿度："
-                        + d["data"]["shidu"]
-                        + "\n空气质量："
-                        + d["data"]["quality"]
-                        + "\nPM2.5："
-                        + str(d["data"]["pm25"])
-                        + "\nPM10："
-                        + str(d["data"]["pm10"])
-                        + "\n风力风向："
-                        + d["data"]["forecast"][0]["fx"]
-                        + " "
-                        + d["data"]["forecast"][0]["fl"]
-                        + "\n感冒指数："
-                        + d["data"]["ganmao"]
-                        + "\n温馨提示："
-                        + d["data"]["forecast"][0]["notice"]
-                        + "\n更新时间："
-                        + d["time"]
+                    "城市："
+                    + d["cityInfo"]["parent"]
+                    + " "
+                    + d["cityInfo"]["city"]
+                    + "\n日期："
+                    + d["data"]["forecast"][0]["ymd"]
+                    + " "
+                    + d["data"]["forecast"][0]["week"]
+                    + "\n天气："
+                    + d["data"]["forecast"][0]["type"]
+                    + "\n温度："
+                    + d["data"]["forecast"][0]["high"]
+                    + " "
+                    + d["data"]["forecast"][0]["low"]
+                    + "\n湿度："
+                    + d["data"]["shidu"]
+                    + "\n空气质量："
+                    + d["data"]["quality"]
+                    + "\nPM2.5："
+                    + str(d["data"]["pm25"])
+                    + "\nPM10："
+                    + str(d["data"]["pm10"])
+                    + "\n风力风向："
+                    + d["data"]["forecast"][0]["fx"]
+                    + " "
+                    + d["data"]["forecast"][0]["fl"]
+                    + "\n感冒指数："
+                    + d["data"]["ganmao"]
+                    + "\n温馨提示："
+                    + d["data"]["forecast"][0]["notice"]
+                    + "\n更新时间："
+                    + d["time"]
                 )
             msg_all += msg + "\n\n"
         return msg_all
--- utils_ver.py
+++ utils_ver.py
@@ -5,7 +5,7 @@ import time
 
 import requests
 
-__version__ = "20220411-4-049"
+__version__ = "20220430-4-011"
 ONLINE_VERSION = ""
 ver_re = re.compile("__version__ = .(\\d+-\\d+-...).")
 
